3ee30ceb5a49ad21cd8510f15f78c05c602f6f31
FelipeReis888 <maylsonfelipe43@gmail.com>
2025-07-09 08:01:07 -0300

    Create Atividade4parte2.java

---
+++ Atividade4parte2.java
@@ -0,0 +1,10 @@
+public class ComparacaoIdades {
+    public static void main(String[] args) {
+        int idadePessoa1 = 25;
+        int idadePessoa2 = 30;
+      
+        boolean pessoa1MaisVelha = idadePessoa1 > idadePessoa2;
+
+        System.out.println("A primeira pessoa é mais velha: " + pessoa1MaisVelha);
+    }
+}
