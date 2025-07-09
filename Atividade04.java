367fffd5c79aefb4927df00023d8f675abf8de00
FelipeReis888 <maylsonfelipe43@gmail.com>
2025-07-09 08:00:00 -0300

    Create Atividade04.java

---
+++ Atividade04.java
@@ -0,0 +1,13 @@
+public class Retangulo {
+    public static void main(String[] args) {
+        double base = 5.0;
+        double altura = 3.0;
+
+        double area = base * altura;
+
+        double perimetro = 2 * (base + altura);
+
+        System.out.println("Área do retângulo: " + area);
+        System.out.println("Perímetro do retângulo: " + perimetro);
+    }
+}
